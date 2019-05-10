86332bf9f3e0252ad991ad42d90c77aba2615bd0
Pouya <pouya.mozafar@gmail.com>
2019-05-10 14:40:28 +0430

    initial structure and commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import TheatreRadioButton from './components/TheatreRadioButton';
+
+function App() {
+  return (
+    <div className="App">
+      <TheatreRadioButton />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/TheatreRadioButton/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class TheatreRadioButton extends React.PureComponent {
+  componentDidMount() {
+    this.state = {
+
+    };
+  }
+
+  render() {
+    return (
+      <div />
+    );
+  }
+}
+
+export default TheatreRadioButton;
